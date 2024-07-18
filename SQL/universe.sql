8c982cf5c3d6bee2590a5ce00335f40ca79b30df
Victor <144251687+1Kesty@users.noreply.github.com>
2024-07-18 13:29:54 -0300

    Add files via upload

--- SQL/universe.sql
+++ SQL/universe.sql
@@ -11,7 +11,10 @@ SET row_security = off;
 
 DROP DATABASE universe;
 
+
 CREATE DATABASE universe WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';
+
+
 ALTER DATABASE universe OWNER TO freecodecamp;
 
 \connect universe
@@ -28,7 +31,10 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 SET default_tablespace = '';
+
 SET default_table_access_method = heap;
+
+
 
 CREATE TABLE public.galaxy (
     galaxy_id integer NOT NULL,
@@ -41,7 +47,11 @@ CREATE TABLE public.galaxy (
     has_life boolean,
     has_water boolean
 );
+
+
 ALTER TABLE public.galaxy OWNER TO freecodecamp;
+
+
 
 CREATE SEQUENCE public.galaxy_galaxy_id_seq
     AS integer
@@ -50,8 +60,16 @@ CREATE SEQUENCE public.galaxy_galaxy_id_seq
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
+
+
 ALTER TABLE public.galaxy_galaxy_id_seq OWNER TO freecodecamp;
+
+
+
 ALTER SEQUENCE public.galaxy_galaxy_id_seq OWNED BY public.galaxy.galaxy_id;
+
+
+
 
 CREATE TABLE public.moon (
     moon_id integer NOT NULL,
@@ -64,7 +82,11 @@ CREATE TABLE public.moon (
     has_life boolean,
     has_water boolean
 );
+
+
 ALTER TABLE public.moon OWNER TO freecodecamp;
+
+
 
 CREATE SEQUENCE public.moon_moon_id_seq
     AS integer
@@ -73,8 +95,16 @@ CREATE SEQUENCE public.moon_moon_id_seq
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
+
+
 ALTER TABLE public.moon_moon_id_seq OWNER TO freecodecamp;
+
+
+
 ALTER SEQUENCE public.moon_moon_id_seq OWNED BY public.moon.moon_id;
+
+
+
 
 CREATE TABLE public.more_info (
     more_info_id integer NOT NULL,
@@ -82,7 +112,11 @@ CREATE TABLE public.more_info (
     name character varying(20) NOT NULL,
     description text
 );
+
+
 ALTER TABLE public.more_info OWNER TO freecodecamp;
+
+
 
 CREATE SEQUENCE public.more_info_more_info_id_seq
     AS integer
@@ -91,8 +125,16 @@ CREATE SEQUENCE public.more_info_more_info_id_seq
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
+
+
 ALTER TABLE public.more_info_more_info_id_seq OWNER TO freecodecamp;
+
+
+
 ALTER SEQUENCE public.more_info_more_info_id_seq OWNED BY public.more_info.more_info_id;
+
+
+
 
 CREATE TABLE public.planet (
     planet_id integer NOT NULL,
@@ -106,7 +148,11 @@ CREATE TABLE public.planet (
     has_life boolean,
     has_water boolean
 );
+
+
 ALTER TABLE public.planet OWNER TO freecodecamp;
+
+
 
 CREATE SEQUENCE public.planet_planet_id_seq
     AS integer
@@ -115,8 +161,15 @@ CREATE SEQUENCE public.planet_planet_id_seq
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
+
+
 ALTER TABLE public.planet_planet_id_seq OWNER TO freecodecamp;
+
+
 ALTER SEQUENCE public.planet_planet_id_seq OWNED BY public.planet.planet_id;
+
+
+
 
 CREATE TABLE public.star (
     star_id integer NOT NULL,
@@ -130,7 +183,11 @@ CREATE TABLE public.star (
     has_life boolean,
     has_water boolean
 );
+
+
 ALTER TABLE public.star OWNER TO freecodecamp;
+
+
 
 CREATE SEQUENCE public.star_star_id_seq
     AS integer
@@ -139,14 +196,40 @@ CREATE SEQUENCE public.star_star_id_seq
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
+
+
 ALTER TABLE public.star_star_id_seq OWNER TO freecodecamp;
+
+
+
 ALTER SEQUENCE public.star_star_id_seq OWNED BY public.star.star_id;
 
+
+
+
 ALTER TABLE ONLY public.galaxy ALTER COLUMN galaxy_id SET DEFAULT nextval('public.galaxy_galaxy_id_seq'::regclass);
+
+
+
 ALTER TABLE ONLY public.moon ALTER COLUMN moon_id SET DEFAULT nextval('public.moon_moon_id_seq'::regclass);
+
+
+
+
 ALTER TABLE ONLY public.more_info ALTER COLUMN more_info_id SET DEFAULT nextval('public.more_info_more_info_id_seq'::regclass);
+
+
+
+
 ALTER TABLE ONLY public.planet ALTER COLUMN planet_id SET DEFAULT nextval('public.planet_planet_id_seq'::regclass);
+
+
+
+
 ALTER TABLE ONLY public.star ALTER COLUMN star_id SET DEFAULT nextval('public.star_star_id_seq'::regclass);
+
+
+
 
 INSERT INTO public.galaxy VALUES (1, 1, 'galaxy1', 500, 750, 1500.75, 'solid', true, true);
 INSERT INTO public.galaxy VALUES (2, 2, 'galaxy2', 500, 750, 1500.75, 'solid', true, true);
@@ -154,6 +237,8 @@ INSERT INTO public.galaxy VALUES (3, 3, 'galaxy3', 500, 750, 1500.75, 'solid', t
 INSERT INTO public.galaxy VALUES (4, 4, 'galaxy4', 500, 750, 1500.75, 'solid', true, true);
 INSERT INTO public.galaxy VALUES (5, 5, 'galaxy5', 500, 750, 1500.75, 'solid', true, true);
 INSERT INTO public.galaxy VALUES (6, 6, 'galaxy6', 500, 750, 1500.75, 'solid', true, true);
+
+
 
 INSERT INTO public.moon VALUES (1, 1, 'moon1', 500, 750, 1500.75, 'true', true, NULL);
 INSERT INTO public.moon VALUES (2, 2, 'moon2', 500, 750, 1500.75, 'true', true, NULL);
@@ -176,37 +261,149 @@ INSERT INTO public.moon VALUES (18, 11, 'moon18', 500, 750, 1500.75, 'true', tru
 INSERT INTO public.moon VALUES (19, 11, 'moon19', 500, 750, 1500.75, 'true', true, NULL);
 INSERT INTO public.moon VALUES (20, 11, 'moon20', 500, 750, 1500.75, 'true', true, NULL);
 
+
+
 INSERT INTO public.more_info VALUES (1, 1, 'info1', 'lorem impsum');
 INSERT INTO public.more_info VALUES (2, 2, 'info2', 'lorem impsum');
 INSERT INTO public.more_info VALUES (3, 3, 'info3', 'lorem impsum');
 INSERT INTO public.more_info VALUES (4, 4, 'info4', 'lorem impsum');
 INSERT INTO public.more_info VALUES (5, 5, 'info5', 'lorem impsum');
-INSERT INTO public.more_info VALUES (6, 6, 'info6', 'lorem impsum');
-INSERT INTO public.more_info VALUES (7, 7, 'info7', 'lorem impsum');
-INSERT INTO public.more_info VALUES (8, 8, 'info8', 'lorem impsum');
-INSERT INTO public.more_info VALUES (9, 9, 'info9', 'lorem impsum');
-INSERT INTO public.more_info VALUES (10, 10, 'info10', 'lorem impsum');
-INSERT INTO public.more_info VALUES (11, 11, 'info11', 'lorem impsum');
-INSERT INTO public.more_info VALUES (12, 12, 'info12', 'lorem impsum');
-INSERT INTO public.more_info VALUES (13, 13, 'info13', 'lorem impsum');
-INSERT INTO public.more_info VALUES (14, 14, 'info14', 'lorem impsum');
-INSERT INTO public.more_info VALUES (15, 15, 'info15', 'lorem impsum');
-INSERT INTO public.more_info VALUES (16, 16, 'info16', 'lorem impsum');
-INSERT INTO public.more_info VALUES (17, 17, 'info17', 'lorem impsum');
-INSERT INTO public.more_info VALUES (18, 18, 'info18', 'lorem impsum');
-INSERT INTO public.more_info VALUES (19, 19, 'info19', 'lorem impsum');
-INSERT INTO public.more_info VALUES (20, 20, 'info20', 'lorem impsum');
 
-INSERT INTO public.planet VALUES (1, 1, 1, 'planet1', 500, 750, 1500.75, 'solid', true, true);
-INSERT INTO public.planet VALUES (2, 2, 2, 'planet2', 500, 750, 1500.75, 'solid', true, true);
-INSERT INTO public.planet VALUES (3, 3, 3, 'planet3', 500, 750, 1500.75, 'solid', true, true);
-INSERT INTO public.planet VALUES (4, 4, 4, 'planet4', 500, 750, 1500.75, 'solid', true, true);
-INSERT INTO public.planet VALUES (5, 5, 5, 'planet5', 500, 750, 1500.75, 'solid', true, true);
-INSERT INTO public.planet VALUES (6, 6, 6, 'planet6', 500, 750, 1500.75, 'solid', true, true);
 
-INSERT INTO public.star VALUES (1, 1, 1, 'star1', 500, 750, 1500.75, 'solid', true, true);
-INSERT INTO public.star VALUES (2, 2, 2, 'star2', 500, 750, 1500.75, 'solid', true, true);
-INSERT INTO public.star VALUES (3, 3, 3, 'star3', 500, 750, 1500.75, 'solid', true, true);
-INSERT INTO public.star VALUES (4, 4, 4, 'star4', 500, 750, 1500.75, 'solid', true, true);
-INSERT INTO public.star VALUES (5, 5, 5, 'star5', 500, 750, 1500.75, 'solid', true, true);
-INSERT INTO public.star VALUES (6, 6, 6, 'star6', 500, 750, 1500.75, 'solid', true, true);
+
+INSERT INTO public.planet VALUES (1, 1, 1, 'planet1', 500, 750, 1500.75, 'true', true, NULL);
+INSERT INTO public.planet VALUES (2, 2, 2, 'planet2', 500, 750, 1500.75, 'true', true, NULL);
+INSERT INTO public.planet VALUES (3, 3, 3, 'planet3', 500, 750, 1500.75, 'true', true, NULL);
+INSERT INTO public.planet VALUES (4, 4, 4, 'planet4', 500, 750, 1500.75, 'true', true, NULL);
+INSERT INTO public.planet VALUES (5, 5, 5, 'planet5', 500, 750, 1500.75, 'true', true, NULL);
+INSERT INTO public.planet VALUES (6, 6, 6, 'planet6', 500, 750, 1500.75, 'true', true, NULL);
+INSERT INTO public.planet VALUES (7, 6, 7, 'planet7', 500, 750, 1500.75, 'true', true, NULL);
+INSERT INTO public.planet VALUES (8, 6, 8, 'planet8', 500, 750, 1500.75, 'true', true, NULL);
+INSERT INTO public.planet VALUES (9, 6, 9, 'planet9', 500, 750, 1500.75, 'true', true, NULL);
+INSERT INTO public.planet VALUES (10, 6, 10, 'planet10', 500, 750, 1500.75, 'true', true, NULL);
+INSERT INTO public.planet VALUES (11, 6, 11, 'planet11', 500, 750, 1500.75, 'true', true, NULL);
+INSERT INTO public.planet VALUES (12, 6, 12, 'planet12', 500, 750, 1500.75, 'true', true, NULL);
+
+
+
+
+INSERT INTO public.star VALUES (1, 1, 1, 'star1', 500, 750, 1500.75, 'true', true, NULL);
+INSERT INTO public.star VALUES (2, 2, 2, 'star2', 500, 750, 1500.75, 'true', true, NULL);
+INSERT INTO public.star VALUES (3, 3, 3, 'star3', 500, 750, 1500.75, 'true', true, NULL);
+INSERT INTO public.star VALUES (4, 4, 4, 'star4', 500, 750, 1500.75, 'true', true, NULL);
+INSERT INTO public.star VALUES (5, 5, 5, 'star5', 500, 750, 1500.75, 'true', true, NULL);
+INSERT INTO public.star VALUES (6, 6, 6, 'star6', 500, 750, 1500.75, 'true', true, NULL);
+
+
+
+
+SELECT pg_catalog.setval('public.galaxy_galaxy_id_seq', 1, false);
+
+
+
+
+SELECT pg_catalog.setval('public.moon_moon_id_seq', 1, false);
+
+
+
+
+SELECT pg_catalog.setval('public.more_info_more_info_id_seq', 1, false);
+
+
+
+
+SELECT pg_catalog.setval('public.planet_planet_id_seq', 1, false);
+
+
+
+
+SELECT pg_catalog.setval('public.star_star_id_seq', 1, false);
+
+
+
+ALTER TABLE ONLY public.galaxy
+    ADD CONSTRAINT galaxy_name_key UNIQUE (name);
+
+
+
+
+ALTER TABLE ONLY public.galaxy
+    ADD CONSTRAINT galaxy_pkey PRIMARY KEY (galaxy_id);
+
+
+
+
+ALTER TABLE ONLY public.moon
+    ADD CONSTRAINT moon_name_key UNIQUE (name);
+
+
+
+
+ALTER TABLE ONLY public.moon
+    ADD CONSTRAINT moon_pkey PRIMARY KEY (moon_id);
+
+
+
+ALTER TABLE ONLY public.more_info
+    ADD CONSTRAINT more_info_name_key UNIQUE (name);
+
+
+
+
+ALTER TABLE ONLY public.more_info
+    ADD CONSTRAINT more_info_pkey PRIMARY KEY (more_info_id);
+
+
+
+ALTER TABLE ONLY public.planet
+    ADD CONSTRAINT planet_name_key UNIQUE (name);
+
+
+
+
+ALTER TABLE ONLY public.planet
+    ADD CONSTRAINT planet_pkey PRIMARY KEY (planet_id);
+
+
+
+ALTER TABLE ONLY public.star
+    ADD CONSTRAINT star_name_key UNIQUE (name);
+
+
+
+ALTER TABLE ONLY public.star
+    ADD CONSTRAINT star_pkey PRIMARY KEY (star_id);
+
+
+
+
+ALTER TABLE ONLY public.galaxy
+    ADD CONSTRAINT galaxy_star_id_fkey FOREIGN KEY (star_id) REFERENCES public.star(star_id);
+
+
+
+ALTER TABLE ONLY public.moon
+    ADD CONSTRAINT moon_planet_id_fkey FOREIGN KEY (planet_id) REFERENCES public.planet(planet_id);
+
+
+
+ALTER TABLE ONLY public.planet
+    ADD CONSTRAINT planet_moon_id_fkey FOREIGN KEY (moon_id) REFERENCES public.moon(moon_id);
+
+
+
+ALTER TABLE ONLY public.planet
+    ADD CONSTRAINT planet_star_id_fkey FOREIGN KEY (star_id) REFERENCES public.star(star_id);
+
+
+
+ALTER TABLE ONLY public.star
+    ADD CONSTRAINT star_galaxy_id_fkey FOREIGN KEY (galaxy_id) REFERENCES public.galaxy(galaxy_id);
+
+
+
+ALTER TABLE ONLY public.star
+    ADD CONSTRAINT star_planet_id_fkey FOREIGN KEY (planet_id) REFERENCES public.planet(planet_id);
+
+
+
